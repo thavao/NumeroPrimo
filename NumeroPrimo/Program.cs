7d6c1682c8017e61a48cd79fa771f384075c262b
thavao <othavioact@gmail.com>
2024-04-19 16:05:31 -0300

    Add project files.

---
+++ NumeroPrimo/Program.cs
@@ -0,0 +1,26 @@
+﻿//1 - faça um programa que leia um numero e informe se esse numero é primo.
+
+int numero;
+bool ehPrimo = true;
+
+Console.WriteLine("--Descubra se um número é primo--");
+Console.Write("Digite um número: ");
+
+numero = int.Parse(Console.ReadLine());
+
+for (int i = 2; i < numero; i++)
+{
+    if (numero % i == 0)
+    {
+        ehPrimo = false;
+        break;
+    }
+}
+
+if (ehPrimo == true && numero != 1)
+    Console.WriteLine($"O número {numero} é primo");
+else
+    Console.WriteLine($"O número {numero} não é primo");
+
+Console.WriteLine("Pressione Enter para encerrar...");
+Console.ReadLine();
